ad4c6580aaaff4a845d76d8d4335b5e89aa2e1ff
Andreo Van Onckelen <andreo@inuits.eu>
2023-02-14 14:42:01 +0100

    added functions for API and removed docker files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
+crt_data = "/home/andreo/Inuits/certiman/certs/example.localhost.crt"
+pk = "/home/andreo/Inuits/certiman/certs/example.localhost.key"
+
+
 @app.get("/")
-def read_main():
-    return {"message":"Hello World"}
+def home():
+    return "Welcome to the certificate API. Go to /certificate to get the certificate details"
+
+
+
+@app.get("/certificate")
+async def get_cert():
+    with open(crt_data, "rb") as f, open(pk, "rb") as f2:
+        crt_bytes = f.read()
+        pk_bytes = f2.read()
+    return {"crt_data": crt_bytes, "private_key": pk_bytes}
+
